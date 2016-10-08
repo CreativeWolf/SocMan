336217496d456e5e0dedd8cc8c4129dc19e06905
CreativeWolf <creativewolf@gmail.com>
2016-10-08 05:46:56 +0530

    0.0.3

--- www/js/index.js
+++ www/js/index.js
@@ -34,7 +34,6 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         
-        keysDB = window.sqlitePlugin.openDatabase({name: 'seckeys.db', location: 'default'});
         app.receivedEvent('deviceready');
 
     },
